c9d100445534a1ca4458a60e7abd36423c697041
Britton <brittonkdeets@gmail.com>
2019-11-16 10:30:42 -0500

    file upload halfway

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent implements OnInit {
+  uploadedFile: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.uploadedFile = {
+      name: '~/dx_salaries.csv'
+    }
+  }
+
+  handleFileUpload(){
+    this.changeFileInputPlaceholderTextColor();
+    const file = document.getElementById('inputGroupFile01').files[0];
+    console.log(file);
+    this.uploadedFile = file;
+  }
+  changeFileInputPlaceholderTextColor(){
+    const elem = document.getElementById('fileInputPlaceholder');
+    elem.style.color = "black";
   }
 
 }
