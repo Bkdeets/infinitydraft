ba8dcda7eaefce62f17aab5f1c877fe2bb239c60
Britton <brittonkdeets@gmail.com>
2019-11-22 19:45:38 -0500

    copy ids

--- src/app/components/team-display/team-display.component.ts
+++ src/app/components/team-display/team-display.component.ts
@@ -14,4 +14,49 @@ export class TeamDisplayComponent implements OnInit {
   ngOnInit() {
   }
 
+  copy(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+  copyIds(){
+    let format = ['F','F','M','M','D','D','GK','UTIL']
+    let pos = {
+      F: [],
+      M: [],
+      D: [],
+      GK: [],
+      UTIL: []
+    }
+    let players = this.team.team;
+    for(let player of players) {
+      pos[player.Position].push(player.Id)
+    }
+    let csvString = '';
+    let index = 0;
+    for (var prop in pos) {
+      if (Object.prototype.hasOwnProperty.call(pos, prop)) {
+          for(let id of pos[prop]){
+            if(index == 0){
+              csvString += id;
+              index += 1;
+            }
+            else {
+              csvString += ','+id;
+            }
+          }
+      }
+    }
+    this.copy(csvString);
+  }
+
 }
